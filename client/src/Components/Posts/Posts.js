2a38516a522a20718b22a5200bdb45541ebadd86
Chijioke Chika <8pluscreative@gmail.com>
2022-11-04 17:50:09 +0100

    FE

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Post from "./Post/Post";
+import { useSelector } from "react-redux";
+
 import useStyles from "./styles";
 
 const Posts = () => {
   const classes = useStyles();
+  const loadedPosts = useSelector((state) => state.posts);
+
+  console.log(loadedPosts);
+
   return (
     <>
       <h1>POSTS</h1>
--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://mern_gee:Achiever25@cluster0.l7yrx98.mongodb.net/?retryWrites=true&w=majority";
