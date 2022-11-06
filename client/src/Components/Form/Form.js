fa8848163e7d38b633521973c06fce90cbcd58f1
Chijioke Chika <8pluscreative@gmail.com>
2022-11-06 11:37:22 +0100

    Fixed bug

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -4,7 +4,7 @@ import FileBase from "react-file-base64";
 import { useDispatch } from "react-redux";
 
 import useStyles from "./styles";
-import { createPost } from "../../api";
+import { createPost } from "../../actions/posts";
 
 const Form = () => {
   const [postData, setPostData] = useState({
@@ -75,7 +75,7 @@ const Form = () => {
           <FileBase
             type="file"
             multiple={false}
-            onDone={(base64) =>
+            onDone={({ base64 }) =>
               setPostData({ ...postData, selectedFile: base64 })
             }
           />
