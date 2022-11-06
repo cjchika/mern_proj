17621faff4669af23eb4caf5871aef836bf1d827
Chijioke Chika <8pluscreative@gmail.com>
2022-11-06 14:47:17 +0100

    On the move

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -1,21 +1,31 @@
 import React from "react";
 import Post from "./Post/Post";
+import { Grid, CircularProgress } from "@material-ui/core";
 import { useSelector } from "react-redux";
 
 import useStyles from "./styles";
 
-const Posts = () => {
+const Posts = ({ setCurrentId }) => {
   const classes = useStyles();
   const loadedPosts = useSelector((state) => state.posts);
 
   console.log(loadedPosts);
 
-  return (
-    <>
-      <h1>POSTS</h1>
-      <Post />
-      <Post />
-    </>
+  return !loadedPosts.length ? (
+    <CircularProgress />
+  ) : (
+    <Grid
+      className={classes.mainContainer}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {loadedPosts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
